db0b156abdccb5dd0f2dfe1a726e170d261a55a9
Leon Markovich <leonmarkovich97@gmail.com>
2021-03-01 16:02:23 +0200

    added some classes for board

---
+++ include/Board.h
@@ -0,0 +1,18 @@
+#include <vector>
+#include<iostream>
+#include <SFML/Graphics.hpp>
+#pragma once
+class Board{
+
+
+public:
+
+
+
+private:
+
+	//sf::Vector 2im_Chess_Board;
+
+
+
+};
---
+++ include/Tile.h
@@ -0,0 +1,18 @@
+#include <vector>
+#include<iostream>
+#include <SFML/Graphics.hpp>
+
+class Tile {
+
+public:
+
+	Tile();
+	~Tile();
+
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~private memeber~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+private:
+
+		sf:Vector2i m_Size
+		sf:Recatangle m_Tile;
+};
