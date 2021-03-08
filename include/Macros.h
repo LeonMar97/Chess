45af55cdc4620fb334f56b5c440aa8e4d4577700
Leon Markovich <leonmarkovich97@gmail.com>
2021-03-08 10:34:29 +0200

    fixed texture singelton to a  better format with loop

--- include/Macros.h
+++ include/Macros.h
@@ -2,8 +2,8 @@
 
 //const sf::Vector2f board_Start(560.f, 100.f);
 const sf::Vector2f board_Start(0.f, 0.f);
-const sf::Vector2f toolSize(350.f, 350.f);
-
+//const sf::Vector2f toolSize(333.f, 333.f);
+const float toolSize = 333.f;
 const unsigned int different_Tools = 6;
 
 
--- src/Textures.cpp
+++ src/Textures.cpp
@@ -16,50 +16,15 @@ Textures& Textures::instance() {
 	 //resizing the tools by color..
 	 m_Tool_Tex[white].resize(different_Tools);
 	 m_Tool_Tex[black].resize(different_Tools);
+
 	 
-	 //setting white  texture.. 
-	 m_Tool_Tex[white][king].loadFromImage(im, sf::IntRect(0, 0, toolSize.x,toolSize.y));
-	 
-	 m_Tool_Tex[white][queen].loadFromImage(im, sf::IntRect(320, 0, 350, 350));
-	 
-	 m_Tool_Tex[white][bishop].loadFromImage(im, sf::IntRect(650, 0, 350, 350));
-	 m_Tool_Tex[white][knight].loadFromImage(im, sf::IntRect(1000, 0, 350, 350));
-	 m_Tool_Tex[white][rook].loadFromImage(im, sf::IntRect(1350, 0, 350, 350));
-	 m_Tool_Tex[white][pawn].loadFromImage(im, sf::IntRect(1650, 0, 350, 350));
-
-
-	 m_Tool_Tex[black][king].loadFromImage(im, sf::IntRect(0, 350, toolSize.x, toolSize.y));
-	 m_Tool_Tex[black][queen].loadFromImage(im, sf::IntRect(320, 350, 350, 350));
-	 m_Tool_Tex[black][bishop].loadFromImage(im, sf::IntRect(650, 350, 350, 350));
-	 m_Tool_Tex[black][knight].loadFromImage(im, sf::IntRect(1000, 350, 350, 350));
-	 m_Tool_Tex[black][rook].loadFromImage(im, sf::IntRect(1350, 350, 350, 350));
-	 m_Tool_Tex[black][pawn].loadFromImage(im, sf::IntRect(1650, 350, 350, 350));
-	 /*
-	 m_Tool_Tex[white][knight].loadFromImage(im, sf::IntRect(350, 0, 350, 350));
-
-
-	 m_Tool_Tex[white][rook].loadFromImage(im, sf::IntRect(350, 0, 350, 350));
-
-
-	 m_Tool_Tex[white][pawn].loadFromImage(im, sf::IntRect(350, 0, 350, 350));
-	 */
-
-
-
-	 //setting all the textures to smooth
-	 for (unsigned int i = 0; i < 2; i++) {
-		 for (unsigned int j = 0; j < different_Tools; j++) {
-			 m_Tool_Tex[i][j].setSmooth(true);
+	 for (unsigned int cr = 0; cr < 2; cr++) {
+		 for (unsigned int tl = king; tl < pawn; tl++) {
+	 //m_Tool_Tex[white][king].loadFromImage(im, sf::IntRect(0, 0, toolSize, toolSize));
+			 m_Tool_Tex[cr][tl].loadFromImage(im, sf::IntRect(toolSize*tl, toolSize*cr, toolSize, toolSize));
+			 m_Tool_Tex[cr][tl].setSmooth(true);
 		 }
-	 }
-	 /*
-	 pic.loadFromImage(im, sf::IntRect(0, 0, 350, 350));
-	 pic.loadFromImage(im, sf::IntRect(0, 0, 350, 350));
-	 pic.loadFromImage(im, sf::IntRect(0, 0, 350, 350));
-	 pic.loadFromImage(im, sf::IntRect(0, 0, 350, 350));
-	 */
-	
-
+	}
 	
 	
 			
